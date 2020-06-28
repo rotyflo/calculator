7ac96d860da94accba1c75889e21820b7cf1fa16
RT <rotyflo@gmail.com>
2020-06-27 23:05:22 -0700

    Divide code into functions, add keyboard func, break GUI

--- js/main.js
+++ js/main.js
@@ -1,90 +1,118 @@
 "use strict";
 
-const buttons = {
-  zero: "0",
-  one: "1",
-  two: "2",
-  three: "3",
-  four: "4",
-  five: "5",
-  six: "6",
-  seven: "7",
-  eight: "8",
-  nine: "9",
-  add: "+",
-  subtract: "-",
-  multiply: "*",
-  divide: "/",
-  equals: "",
-  point: "",
-  del: "",
-  percent: "",
-  clear: ""
-};
 let output = document.getElementById("output");
+let operators = /\+|\-|\*|\//;
+let allowedKeys = [
+  "Backspace",
+  "Enter",
+  "=",
+  ".",
+  "%",
+  "*",
+  "/",
+  "+",
+  "-",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "0"
+];
 
-for (let i = 0, keys = Object.keys(buttons); i < keys.length; i++) {
-  let button = document.getElementById(keys[i]);
-  let val = buttons[keys[i]];
+document.addEventListener("keydown", function(e) {
+  let display = output.innerText;
+  let key = e.key;
 
-  onPress(button, val);
-}
-
-function onPress(button, val) {
-  button.addEventListener("click", function () {
-    let operators = /\+|\-|\*|\//;
-    let current = output.innerText;
-    let strFromLastPoint = current.slice(current.lastIndexOf("."));
-    let lastChar = current.slice(-1);
-
-    switch (button) {
-      case clear:
-        output.innerText = "0";
+  if (allowedKeys.indexOf(key) !== -1) {
+    switch (key) {
+      case "Backspace":
+        clear();
         break;
 
-      case del:
-        output.innerText = current.length === 1 ? "0" : current.slice(0, -1);
+      case "%":
+        percentify();
         break;
 
-      case percent:
-        // roundTo: 2 is hundredths, 4 is millionths, etc
-        let roundTo = current.indexOf(".") === -1 ? 2 : current.length - current.indexOf(".") + 1;
-
-        output.innerText = (current / 100).toFixed(roundTo);
-        //current = output.innerText;
+      case "Enter":
+      case "=":
+        solve();
         break;
 
-      case equals:
-        output.innerText = eval(current);
-
-        if (current === "Infinity" || current === "-Infinity") output.innerText = "Undefined";
+      case ".":
+        decimate();
         break;
 
-      case point:
-        if (current.lastIndexOf(".") === -1) {
-          output.innerText += lastChar.match(operators) ? "0." : ".";
-        }
-        else if (strFromLastPoint.match(operators)) {
-          output.innerText += lastChar.match(operators) ? "0." : ".";
-        }
-        break;
+      case "-":
+        if (makeNegativeIfZero() === "break") break;
 
-      case subtract:
-        if (current === "0") {
-          output.innerText = "-";
-          break;
-        }
-      case add:
-      case multiply:
-      case divide:
-        if (current === "-" || current === "0") break;
-        else if (lastChar.match(operators) || lastChar.match(/\./)) {
-          output.innerText = current.slice(0, -1) + val;
-          break;
-        }
+      case "+":
+      case "*":
+      case "/":
+        if (checkForBreakage(key) === "break") break;
 
       default:
-        output.innerText = current === "0" ? val : current + val;
+        output.innerText = display === "0" ? key : display + key;
     }
-  });
+  }
+});
+
+document.getElementById("clear").addEventListener("click", clear);
+document.getElementById("percentify").addEventListener("click", percentify);
+document.getElementById("solve").addEventListener("click", solve);
+
+function clear() {
+  output.innerText = "0";
+}
+
+function percentify() {
+  let current = output.innerText;
+  let roundTo = current.indexOf(".") === -1 ? 2 : current.length - current.indexOf(".") + 1;
+
+  output.innerText = (current / 100).toFixed(roundTo);
+  //current = output.innerText;
+}
+
+function solve() {
+  let current = output.innerText;
+
+  output.innerText = eval(current);
+
+  if (current === "Infinity" || current === "-Infinity") output.innerText = "Undefined";
+}
+
+function decimate() {
+  let current = output.innerText;
+  let lastChar = current.slice(-1);
+  let strFromLastPoint = current.slice(current.lastIndexOf("."));
+
+  if (current.lastIndexOf(".") === -1) {
+    output.innerText += lastChar.match(operators) ? "0." : ".";
+  }
+  else if (strFromLastPoint.match(operators)) {
+    output.innerText += lastChar.match(operators) ? "0." : ".";
+  }
+}
+
+function makeNegativeIfZero() {
+  let current = output.innerText;
+  if (current === "0") {
+    output.innerText = "-";
+    return "break";
+  }
+}
+
+function checkForBreakage(key) {
+  let current = output.innerText;
+  let lastChar = current.slice(-1);
+
+  if (current === "-" || current === "0") return "break";
+  else if (lastChar.match(operators) || lastChar.match(/\./)) {
+    output.innerText = current.slice(0, -1) + key;
+    return "break";
+  }
 }
