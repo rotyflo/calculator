ed0b24e4be94646e77053361fc41ef7a8e02bc14
RT <me@rtflores.com>
2021-10-21 14:58:49 -0700

    remove useless constructor

--- src/app/components/Buttons.js
+++ src/app/components/Buttons.js
@@ -2,10 +2,6 @@ import React from "react";
 import Button from "./Button";
 
 export default class Buttons extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
 	render() {
 		let process = this.props.process;
 
