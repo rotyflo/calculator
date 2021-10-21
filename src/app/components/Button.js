4632cc3a496afee8c460b547374ecf6052fc3808
RT <me@rtflores.com>
2021-10-21 14:58:27 -0700

    squish Button class with variables

--- src/app/components/Button.js
+++ src/app/components/Button.js
@@ -2,8 +2,12 @@ import React from "react";
 
 export default class Button extends React.Component {
 	render() {
+		let id = this.props.id;
+		let value = this.props.value;
+		let process = this.props.process;
+
 		return (
-			<button id={this.props.id} value={this.props.value} onClick={() => this.props.process(this.props.value)}>{this.props.value}</button>
+			<button id={id} value={value} onClick={() => process(value)}>{value}</button>
 		);
 	}
 }
