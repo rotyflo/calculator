d27a18429073eb47a1c1b17c74d3600a21031e27
RT <me@rtflores.com>
2021-10-21 22:29:41 -0700

    state length limit only once

--- src/app/functions/process.js
+++ src/app/functions/process.js
@@ -1,35 +1,36 @@
 import handlePercent from "./handlePercent";
-import solve from "./solve";
 import handleDecimal from "./handleDecimal";
 import handleOperator from "./handleOperator";
+import solve from "./solve";
 
-const process = (key, output) => {
-  if (!output) return "0"; // <<== hacky bug fix, address soon
-    switch (key) {
-      case "C":
-        return "0";
+const process = (key, output) => {  
+  switch (key) {
+    case "C":
+      return "0";
 
-      case "%":
-        if (output.length > 16) return output; // keep from leaking
-        return handlePercent(output);
+    case "Enter":
+    case "=":
+      return solve(output);
+  }
 
-      case "Enter":
-      case "=":
-        return solve(output);
+  if (output.length > 16) return output; // keep from leaking
+  
+  switch (key) {
+    case "%":
+      return handlePercent(output);
 
-      case ".":
-        return handleDecimal(output);
+    case ".":
+      return handleDecimal(output);
 
-      case "+":
-      case "*":
-      case "/":
-      case "-":
-        return handleOperator(key, output);
+    case "+":
+    case "*":
+    case "/":
+    case "-":
+      return handleOperator(key, output);
 
-      default:
-        if (output.length > 16) return output; // keep from leaking
-        return output === "0" ? key : output + key;
-    }
+    default:
+      return output === "0" ? key : output + key;
+  }
 }
 
 export default process;
