b8f47a1f7bfb9efae0909bb59e2c0c1c3eb97ce8
RT <me@rtflores.com>
2021-10-21 22:17:07 -0700

    fix decimal crashes

--- src/app/functions/handleDecimal.js
+++ src/app/functions/handleDecimal.js
@@ -1,5 +1,14 @@
 const handleDecimal = (output) => {
-  return output.includes(".") ? output : output + ".";
+  let operators = /\+|\-|\*|\//;
+  let decimalEnding = output.slice(-1).match(operators) ? "0." : ".";
+  let strFromLastPoint = output.slice(output.lastIndexOf("."));
+  let operatedSinceDecimal = strFromLastPoint.match(operators);
+  let notExistDecimal = output.lastIndexOf(".") === -1;
+
+  if (notExistDecimal || operatedSinceDecimal) {
+    return output + decimalEnding;
+  }
+  return output;
 }
 
 export default handleDecimal;
