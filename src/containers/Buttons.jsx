15d17b5fb66baa3ebc1bb22eb0036bbdfafbc3e2
RT <me@rtflores.com>
2021-11-02 15:43:43 -0700

    change buttons into a list of objects

--- src/containers/Buttons.jsx
+++ src/containers/Buttons.jsx
@@ -3,71 +3,159 @@ import * as actions from '../actions';
 import { connect } from 'react-redux';
 
 class Buttons extends React.Component {
+	buttons = () => [
+		{
+			id: 'clear',
+			action: this.props.clear,
+			text: 'clear',
+			class: ''
+		},
+		{
+			id: 'percentify',
+			action: this.props.handlePercent,
+			text: '%',
+			class: ''
+		},
+		{
+			id: 'divide',
+			action: () => this.props.handleOperator('/'),
+			text: '\u00F7',
+			class: this.props.operator === '/' ? 'selected-button' : ''
+		},
+		{
+			id: 'seven',
+			action: () => this.props.handleNumber('7'),
+			text: '7',
+			class: ''
+		},
+		{
+			id: 'eight',
+			action: () => this.props.handleNumber('8'),
+			text: '8',
+			class: ''
+		},
+		{
+			id: 'nine',
+			action: () => this.props.handleNumber('9'),
+			text: '9',
+			class: ''
+		},
+		{
+			id: 'multiply',
+			action: () => this.props.handleOperator('*'),
+			text: '\xD7',
+			class: this.props.operator === '*' ? 'selected-button' : ''
+		},
+		{
+			id: 'four',
+			action: () => this.props.handleNumber('4'),
+			text: '4',
+			class: ''
+		},
+		{
+			id: 'five',
+			action: () => this.props.handleNumber('5'),
+			text: '5',
+			class: ''
+		},
+		{
+			id: 'six',
+			action: () => this.props.handleNumber('6'),
+			text: '6',
+			class: ''
+		},
+		{
+			id: 'subtract',
+			action: () => this.props.handleOperator('-'),
+			text: '-',
+			class: this.props.operator === '-' ? 'selected-button' : ''
+		},
+		{
+			id: 'one',
+			action: () => this.props.handleNumber('1'),
+			text: '1',
+			class: ''
+		},
+		{
+			id: 'two',
+			action: () => this.props.handleNumber('2'),
+			text: '2',
+			class: ''
+		},
+		{
+			id: 'three',
+			action: () => this.props.handleNumber('3'),
+			text: '3',
+			class: ''
+		},
+		{
+			id: 'add',
+			action: () => this.props.handleOperator('+'),
+			text: '+',
+			class: this.props.operator === '+' ? 'selected-button' : ''
+		},
+		{
+			id: 'zero',
+			action: () => this.props.handleNumber('0'),
+			text: '0',
+			class: ''
+		},
+		{
+			id: 'decimal',
+			action: this.props.handleDecimal,
+			text: '.',
+			class: ''
+		},
+		{
+			id: 'equals',
+			action: this.props.solve,
+			text: '=',
+			class: ''
+		},
+	];
+
 	render() {
 		return (
 			<div class="buttons">
-				<button id="clear" value="C" onClick={this.props.clear}>clear</button>
-				<button id="percentify" value="%" onClick={this.props.handlePercent}>%</button>
-				<button id="divide" value="/"
-					onClick={() => this.props.handleOperator('/')}
-					className={this.props.operator === '/' ? 'selected-button' : ''}>&#247;</button>
-
-				<button id="seven" value="7" onClick={() => this.props.handleNumber('7')}>7</button>
-				<button id="eight" value="8" onClick={() => this.props.handleNumber('8')}>8</button>
-				<button id="nine" value="9" onClick={() => this.props.handleNumber('9')}>9</button>
-				<button id="multiply" value="*" 
-					onClick={() => this.props.handleOperator('*')}
-					className={this.props.operator === '*' ? 'selected-button' : ''}>&#215;</button>
-
-				<button id="four" value="4" onClick={() => this.props.handleNumber('4')}>4</button>
-				<button id="five" value="5" onClick={() => this.props.handleNumber('5')}>5</button>
-				<button id="six" value="6" onClick={() => this.props.handleNumber('6')}>6</button>
-				<button id="subtract" value="-"
-					onClick={() => this.props.handleOperator('-')}
-					className={this.props.operator === '-' ? 'selected-button' : ''}>-</button>
-
-				<button id="one" value="1" onClick={() => this.props.handleNumber('1')}>1</button>
-				<button id="two" value="2" onClick={() => this.props.handleNumber('2')}>2</button>
-				<button id="three" value="3" onClick={() => this.props.handleNumber('3')}>3</button>
-				<button id="add" value="+"
-					onClick={() => this.props.handleOperator('+')} 
-					className={this.props.operator === '+' ? 'selected-button' : ''}>+</button>
-
-				<button id="zero" value="0" onClick={() => this.props.handleNumber('0')}>0</button>
-				<button id="decimal" value="." onClick={this.props.handleDecimal}>.</button>
-				<button id="equals" value="=" onClick={this.props.solve}>=</button>
+				{
+					this.buttons().map(btn =>
+						<button id={btn.id} onClick={btn.action} className={btn.class}>
+							{btn.text}
+						</button>
+					)
+				}
 			</div>
 		);
 	}
 }
 
 function mapStateToProps(state) {
-  return {
+	return {
 		operator: state.operator
-  }
+	}
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    handleNumber: (number) => {
-      dispatch(actions.handleNumber(number));
-    },
-    handleOperator: (operator) => {
-      dispatch(actions.handleOperator(operator));
-    },
-    handleDecimal: () => {
-      dispatch(actions.handleDecimal());
-    },
-    handlePercent: () => {
-      dispatch(actions.handlePercent());
-    },
-    solve: () => {
-      dispatch(actions.solve());
-    },
+	return {
+		handleNumber: (number) => {
+			dispatch(actions.handleNumber(number));
+		},
+		handleOperator: (operator) => {
+			dispatch(actions.handleOperator(operator));
+		},
+		handleDecimal: () => {
+			dispatch(actions.handleDecimal());
+		},
+		handlePercent: () => {
+			dispatch(actions.handlePercent());
+		},
+		solve: () => {
+			dispatch(actions.solve());
+		},
 		clear: () => {
 			dispatch(actions.clear());
 		}
-  }
+	}
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Buttons);
